dbcef768e6884f422eb1a84f0e4ed92fb11aa525
demoaccount-rishav <rishavbhowmick.2002.va@gmail.com>
2024-07-31 13:32:57 +0530

    Prefinal Commit

--- server.js
+++ server.js
@@ -68,5 +68,5 @@ io.on('connection', socket => {
 const PORT = 3000;
 
 server.listen(PORT, () => {
-  console.log(`server running at http://localhost:${PORT}`);
+  console.log(`Express server running at http://localhost:${PORT}`);
 });
